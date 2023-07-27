b4dc6c31306bd895f73620688ab7e0fbac6cc6ba
seheichenko-anna <seheichenko.anna@gmail.com>
2023-07-27 15:08:31 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { BookProvider } from "./pages/BookContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <BookProvider>
       <App />
     </BookProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
